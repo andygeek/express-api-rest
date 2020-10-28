b00401723be41c98ef515ca73d5fdc331e0c3b8d
AndyGeek <andy.eulogio@gmail.com>
2020-10-28 13:37:17 -0500

    Finished conecction with mongodb

--- services/products.js
+++ services/products.js
@@ -1,9 +1,15 @@
 const productsMocks = require("../utils/mocks/products");
+const MongoLib = require("../lib/mongo");
 
 class ProductsService {
-  constructor() {}
-  getProducts({ tags }) {
-    return Promise.resolve(productsMocks);
+  constructor() {
+    this.collection = "products";
+    this.mongoDB = new MongoLib();
+  }
+  async getProducts({ tags }) {
+    const query = tags && { tags: { $in: tags } };
+    const products = await this.mongoDB.getAll(this.collection, query);
+    return products || [];
   }
   getProduct({ productId }) {
     return Promise.resolve(productsMocks[0]);
